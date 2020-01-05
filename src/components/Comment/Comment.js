571a2ff665b3d95845550528e4b7785b0b39c46e
nayakunin <nayakunin@outlook.com>
2020-01-05 23:42:39 +0300

    added date support, but still only one digit for day and month

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,12 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Comment.css';
 
 class Comment extends Component {
+    constructor(props) {
+        super(props);
+        let date = new Date(this.props.date);
+        this.state = {
+            date: `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`,
+            text: this.props.text
+        }
+    }
+
     render() {
         return (
             <div className='Comment'>
-                <p className='Comment__date'>{this.props.date}</p>
-                <p className='Comment__content'>{this.props.text}</p>
+                <p className='Comment__date'>{this.state.date}</p>
+                <p className='Comment__content'>{this.state.text}</p>
             </div>
         );
     }
