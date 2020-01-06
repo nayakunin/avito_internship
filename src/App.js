398ddd666936dc616b2a85523cd6a3fde4467ec4
nayakunin <nayakunin@outlook.com>
2020-01-06 23:29:01 +0300

    removed component import from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import MainContent from './components/MainContent/MainContent';
