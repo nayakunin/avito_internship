926b8e113a00ba9f26696dd4a975fe29d403032b
nayakunin <nayakunin@outlook.com>
2020-01-06 23:03:12 +0300

    transformed App component from class to functional component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,102 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import MainContent from './components/MainContent/MainContent';
 import Footer from './components/Footer/Footer';
 import PopupWindow from './components/PopupWindow/PopupWindow';
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			popupIsOpen: false,
-		};
-		this.handlePopupOpen = this.handlePopupOpen.bind(this);
-		this.handlePopupClose = this.handlePopupClose.bind(this);
-	}
+function App() {
+    const [popupIsOpen, setPopupIsOpen] = useState(false);
+    const [scrollY, setScrollY] = useState();
+    const [data, setData] = useState();
+    const [popup, setPopup] = useState(null);
 
-	handlePopupOpen(data) {
-		this.setState({
-			popupIsOpen: true,
-			data,
-		});
-	}
+    useEffect(() => {
+        if (popupIsOpen) {
+            setPopup(
+                <PopupWindow
+                    picture={data}
+                    handlePopupClose={handlePopupClose}
+                />
+            );
 
-	handlePopupClose() {
-		this.setState({
-			popupIsOpen: false,
-		});
-	}
+            document.body.style.position = 'fixed';
+            document.body.style.top = `-${scrollY}px`;
+        } else {
+            const body = document.body;
+            body.style.position = '';
+            body.style.top = '';
+            window.scrollTo(0, scrollY * -1);
+            setPopup(null);
+        }
+    }, [popupIsOpen])
 
-	render() {
-		let popup = this.state.popupIsOpen ?
-			<PopupWindow
-				picture={this.state.data}
-				handlePopupClose={this.handlePopupClose}
-			/> :
-			null;
-		return (
-			<div>
-				<Header />
-				<MainContent handlePopupOpen={this.handlePopupOpen} />
-				<Footer />
-				{popup}
-			</div>
-		);
-	}
+    const handlePopupOpen = (content, scrollPos) => {
+        setScrollY(scrollPos);
+        setData(content);
+        setPopupIsOpen(true);
+    }
+
+    const handlePopupClose = () => {
+        setPopupIsOpen(false);
+    }
+
+    return (
+        <div>
+            <Header />
+            <MainContent handlePopupOpen={handlePopupOpen} />
+            <Footer />
+            {popup}
+        </div>
+    );
 }
+
+// class App extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             popupIsOpen: false,
+//         };
+//         this.handlePopupOpen = this.handlePopupOpen.bind(this);
+//         this.handlePopupClose = this.handlePopupClose.bind(this);
+//     }
+
+//     handlePopupOpen(data, scrollY) {
+//         this.setState({
+//             popupIsOpen: true,
+//             data,
+//         });
+//         document.body.style.position = 'fixed';
+//         document.body.style.top = `-${scrollY}px`;
+//     }
+
+//     handlePopupClose() {
+//         const body = document.body;
+//         const scrollY = body.style.top;
+//         body.style.position = '';
+//         body.style.top = '';
+//         window.scrollTo(0, scrollY * -1);
+//         this.setState({
+//             popupIsOpen: false,
+//         });
+//     }
+
+//     render() {
+//         let popup = this.state.popupIsOpen ?
+//             <PopupWindow
+//                 picture={this.state.data}
+//                 handlePopupClose={this.handlePopupClose}
+//             /> :
+//             null;
+//         return (
+//             <div>
+//                 <Header />
+//                 <MainContent handlePopupOpen={this.handlePopupOpen} />
+//                 <Footer />
+//                 {popup}
+//             </div>
+//         );
+//     }
+// }
 
 export default App;
